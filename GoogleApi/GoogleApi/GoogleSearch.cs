06e41c7a888dcda4adcbb8777f32d31187f9fcce
JoãoLucasBontorinSav <JooLucasBontorinSav@DESKTOP-RDH96Q3>
2017-01-11 11:10:49 -0200

    Project

---
+++ GoogleApi/GoogleApi/GoogleSearch.cs
@@ -0,0 +1,38 @@
+﻿using Google.Apis.Customsearch.v1;
+using Google.Apis.Customsearch.v1.Data;
+using Google.Apis.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleApi
+{
+    class GoogleSearch
+    {
+        //API Key
+        private static string API_KEY = "...";
+
+        //The custom search engine identifier
+        private static string cx = "...";
+
+        public static CustomsearchService Service = new CustomsearchService(
+            new BaseClientService.Initializer
+            {
+                ApplicationName = "IWSocialSearch",
+                ApiKey = API_KEY,
+            });
+        
+        public static IList<Result> Search(string query)
+        {
+          //  Console.WriteLine("CRM NAME: {0} ...", query);
+
+            CseResource.ListRequest listRequest = Service.Cse.List(query);
+            listRequest.Cx = cx;
+
+            Search search = listRequest.Execute();
+            return search.Items;
+        }
+    }
+}
---
+++ GoogleApi/GoogleApi/Program.cs
@@ -0,0 +1,41 @@
+﻿using Google.Apis.Customsearch.v1.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace GoogleApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var query = "joao lucas bontorin linkedin";               // LOOKING FOR THIS CONTENT IN QUERY
+            var results = GoogleSearch.Search(query);
+            if (results != null)
+            {
+                Console.WriteLine("Query: " + query);
+
+                foreach (Result result in results)
+                {                   
+                    if (result.Link.Contains("linkedin.com"))
+                    {
+                        Console.WriteLine("-----------------------------------");
+                        Console.WriteLine("Título:{0}", result.Title);
+                        Console.WriteLine("URL: {0}", result.Link);
+                    }
+                    else
+                    {
+                        Console.Write("Linkedin não encontrado");
+                        Console.WriteLine();
+                    }
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
